b8455e0277ab87534f9ffec571c65f654f738026
DavidMunozMartinez <jdavid_9502@outlook.com>
2022-09-20 02:41:24 -0500

    More responsive design changes

--- src/views/nav-bar/nav-bar.contants.ts
+++ src/views/nav-bar/nav-bar.contants.ts
@@ -1,37 +1,37 @@
 interface IMenuItem {
-  icon: string,
-  name: string,
-  subitems?: ISubItem[],
-  expanded?: boolean
+  icon: string;
+  name: string;
+  subitems?: ISubItem[];
+  expanded?: boolean;
 }
 
 interface ISubItem {
-  name: string,
-  icon?: string
+  name: string;
+  icon?: string;
 }
 
 export const MenuItems: IMenuItem[] = [
   {
     icon: "home-simple-door",
     name: "Home",
-    expanded: false,
+    expanded: true,
     subitems: [
       {
-        name: 'subitem',
-        icon: '',
-      }
-    ]
+        name: "subitem",
+        icon: "",
+      },
+    ],
   },
   {
     icon: "xray-view",
     name: "Devices",
+    expanded: true,
+    subitems: [],
   },
   {
     icon: "",
     name: "Test",
-  },
-  {
-    icon: "",
-    name: "Test",
+    expanded: true,
+    subitems: [],
   }
 ];
