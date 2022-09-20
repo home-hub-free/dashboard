5de5b307e5bae0ed64d7b84a6bf8c8eab1dcc82b
DavidMunozMartinez <jdavid_9502@outlook.com>
2022-09-20 01:05:03 -0500

    Adding new file

---
+++ src/views/nav-bar/nav-bar.contants.ts
@@ -0,0 +1,23 @@
+interface IMenuItem {
+  icon: string,
+  name: string
+}
+
+export const MenuItems: IMenuItem[] = [
+  {
+    icon: "home-simple-door",
+    name: "Home",
+  },
+  {
+    icon: "xray-view",
+    name: "Devices",
+  },
+  {
+    icon: "",
+    name: "",
+  },
+  {
+    icon: "",
+    name: "",
+  }
+];
--- src/views/nav-bar/nav-bar.ts
+++ src/views/nav-bar/nav-bar.ts
@@ -1,21 +1,6 @@
 import { Bind } from "bindrjs";
+import { MenuItems } from "./nav-bar.contants";
 import template from "./nav-bar.html";
-
-interface IItem {
-  icon: string,
-  name: string
-}
-
-const MenuItems: IItem[] = [
-  {
-    icon: "home-simple-door",
-    name: "Home",
-  },
-  {
-    icon: "xray-view",
-    name: "Devices",
-  },
-];
 
 export const NavBar = new Bind({
   id: "nav-bar",
