bfafa7fa20e5671fde9ce1c6d6797a3b59dd3c59
DavidMunozMartinez <jdavid_9502@outlook.com>
2025-08-16 19:29:06 -0600

    Update camera in edit view

--- src/utils/ws-camera-handler.ts
+++ src/utils/ws-camera-handler.ts
@@ -1,31 +1,38 @@
 import io from "socket.io-client/dist/socket.io.js";
 import { DevicesTab } from "../main-content/menu-items/home-menu/tabs/devices/devices-tab";
+import { OverlayModal } from "../overlay-modal/overlay-modal";
 
-const cameraWSEndpoint = 'http://192.168.1.199:8082/';
+const cameraWSEndpoint = "http://192.168.1.199:8082/";
 const cameraWS = io.connect(cameraWSEndpoint);
 
 const feeds: string[] = [];
 
 export function initCameraWS() {
-  cameraWS.on('connect', () => {
+  cameraWS.on("connect", () => {
     console.log("Camera connections started");
   });
 
-  cameraWS.on('error', () => {
-    console.log('Failed');
-  })
+  cameraWS.on("error", () => {
+    console.log("Failed");
+  });
 }
 
 export function subscribeCameraFeed(id?: string) {
   if (!id) return;
   if (feeds.includes(id)) return;
-  
+
   let camera = DevicesTab.bind?.devices?.find((device) => device.id === id);
 
   cameraWS.on(id, (frame) => {
     camera = DevicesTab.bind.devices.find((device) => device.id === id);
     if (camera) {
       camera.value = frame;
+      if (
+        OverlayModal.bind.template &&
+        OverlayModal.bind.data.id === camera.id
+      ) {
+        OverlayModal.bind.data.value = frame;
+      }
     }
-  })
+  });
 }
